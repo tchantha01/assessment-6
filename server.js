dfb0f0279f697bab71c157f46268a2d7b91009bf
Toky Chanthavong <tchantha01@gmail.com>
2022-09-23 15:51:35 -0600

    game works

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const {shuffleArray} = require('./utils')
 
 app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, './public/index.html')))
-app.use(express.static(path.join(__dirname, '../public')))
-
+// Change endpoints in html to match ./index.css and ./index.js
+// app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.get('/styles', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.css'))
 })
 
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.js'))
 })
 
-app.get('/styles', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.css'))
-})
 
 
 app.get('/api/robots', (req, res) => {
