18494cb827d84c7ecde3e865b8cba8a67e07e856
Toky Chanthavong <tchantha01@gmail.com>
2022-09-25 19:26:42 -0600

    unit testing

--- functions.test.ts
+++ functions.test.ts
@@ -2,8 +2,8 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     test('Test should return array equal to argument', () => {
-        expect(shuffleArray([1, 2, 3])).toEqual(arrCopy([1, 2, 3]))
-    }
+        expect(shuffleArray([1, 2, 3])).toEqual([1, 2, 3])
+    })
 
 
 
