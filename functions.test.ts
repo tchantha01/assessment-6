e4d930ec624ab0f2685c817f504dfbf264740d94
Toky Chanthavong <tchantha01@gmail.com>
2022-09-25 22:30:54 -0600

    unit testing failing both test

--- functions.test.ts
+++ functions.test.ts
@@ -1,10 +1,18 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    test('Test should return array equal to argument', () => {
-        expect(shuffleArray([1, 2, 3])).toEqual([1, 2, 3])
+    
+    test('Test should return array length equal to argument length', () => {
+        let arrCopy = [1, 2, 3]
+        expect(shuffleArray.length).toEqual(arrCopy.length)
+    })
+
+    test('Test should return an array', () => {
+        let arrCopy = [1, 2, 3]
+        expect(shuffleArray(arrCopy)).toBe([])
+    })
+
     })
 
 
 
-})
